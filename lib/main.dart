446799fc5ec541bfe49e08fc2bdbd4cd9fcad886
shreeharikulkarni907 <shreehari.cs19@bmsce.ac.in>
2022-03-23 18:35:01 +0530

    INITITAL COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -14,10 +14,50 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
 
-class DicePage extends StatelessWidget {
+
   @override
+  _DicePageState createState() => _DicePageState();
+}
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber=5;
+  int rightdicenumber=2;
+  @override
+  void new_state()
+  {
+    leftdicenumber=Random().nextInt(6);
+    leftdicenumber=leftdicenumber+1;
+
+    rightdicenumber=Random().nextInt(6);
+    rightdicenumber=rightdicenumber+1;
+  }
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                setState(() {
+                  new_state();
+                });
+              },
+              child: Image.asset('images/dice$leftdicenumber.png'),
+            ),
+          ),
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    new_state();
+                  });
+                },
+                child: Image.asset('images/dice$rightdicenumber.png'),
+              )
+          ),
+        ],
+      ),
+    );
   }
 }
